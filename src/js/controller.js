5967749e44893ec9a2f00e19cea2a590716a8b9d
Mpprabhu <prabhu92004@gmail.com>
2024-09-04 01:37:37 +0530

    Day - 46 setting the API and calls

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,32 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// API Calls
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+
+    // if there is response Error
+    if (!response.ok) throw new Error(`${data.message} ${response.status}`);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
